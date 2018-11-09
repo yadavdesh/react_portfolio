128f9336937d2df7c72c6cf857643ddbb818af94
yadavdesh <bhardwajdesh@gmail.com>
2018-11-09 03:20:59 +0100

    Sudoku solver project addition

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -54,6 +54,21 @@ export default class Header extends Component {
               </div>
             </div> {/* item end */}
 
+            <div className="columns portfolio-item">
+              <div className="item-wrap">
+                <a href="#modal-04" title>
+                  <img alt src="images/portfolio/sudoku_solver.jpg" />
+                  <div className="overlay">
+                    <div className="portfolio-item-meta">
+                      <h5>Soduku Puzzle Solver</h5>
+                      <p>Ruby Project</p>
+                    </div>
+                  </div>
+                  <div className="link-icon"><i className="icon-plus" /></div>
+                </a>
+              </div>
+            </div> {/* item end */}
+
           </div> {/* portfolio-wrapper end */}
         </div> {/* twelve columns end */}
         {/* Modal Popup
@@ -92,6 +107,19 @@ export default class Header extends Component {
             <a className="popup-modal-dismiss">Close</a>
           </div>
         </div>{/* modal-03 End */}
+
+        <div id="modal-04" className="popup-modal mfp-hide">
+          <img className="scale-with-grid" src="images/portfolio/modals/soduku_solver.jpg" alt />
+          <div className="description-box">
+            <h4>Sudoku Puzzle Solver</h4>
+            <span className="categories"><i className="fa fa-tag" />Ruby Project</span>
+          </div>
+          <div className="link-box">
+            <a href="https://github.com/yadavdesh/sudoku_solver">Details</a>
+            <a className="popup-modal-dismiss">Close</a>
+          </div>
+        </div>{/* modal-04 End */}
+
       </div> {/* row End */}
     </section>
     </React.Fragment>
